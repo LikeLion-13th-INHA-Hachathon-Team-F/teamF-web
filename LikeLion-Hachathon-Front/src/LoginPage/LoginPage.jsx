367833fdb4483da4582126a5e4f7e0342739e73e
kangm1nse0 <alstj2004102@naver.com>
2025-07-04 13:36:22 +0900

    로그인페이지

--- LikeLion-Hachathon-Front/src/LoginPage/LoginPage.jsx
+++ LikeLion-Hachathon-Front/src/LoginPage/LoginPage.jsx
@@ -1,10 +1,34 @@
 import React from "react";
-
+import "./LoginPage.css";
 function LoginPage() { 
     return(
-        <div>
-            <h1>ㅎㅇ</h1>
+        <div className="LoginPage-Page">
+            <div className="LoginPage-Headline">
+                lastLink
+            </div>
+            <div className="LoginPage-IdInsert">
+                <input
+                type="text"
+                placeholder="아이디를 입력하세요."
+                />
+            </div>
+            <div className="LoginPage-PasswordInsert">
+                <input
+                type="text"
+                placeholder="비밀번호를 입력하세요."
+                />
+            </div>
+            <div className="LoginPage-LoginButton">
+                <button>
+                    로그인 하기 
+                </button>
 
+            </div>
+            <div className="LoginPage-SignupButton">
+                <button>
+                    회원가입 하러가기
+                </button>
+            </div>
         </div>
     ) 
 }
