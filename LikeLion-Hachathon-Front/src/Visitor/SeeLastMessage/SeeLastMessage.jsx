3aab0fa1b14d0f42d2f2128098216073a10fe86c
kangm1nse0 <alstj2004102@naver.com>
2025-07-07 19:47:20 +0900

    수정

--- LikeLion-Hachathon-Front/src/Visitor/SeeLastMessage/SeeLastMessage.jsx
+++ LikeLion-Hachathon-Front/src/Visitor/SeeLastMessage/SeeLastMessage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './SeeLastMessage.css'
+import { useNavigate } from 'react-router-dom'
 
 const SeeLastMessage = () => {
+    const navigate = useNavigate();
   return (
     <div>
-      이거 하면 끝....
+      <div className="AddMemory-Top-bar">
+            <div className='backButton' onClick={() => navigate(-1)}>←</div>
+            <div className="Logo">lastLink</div>
+      </div>
     </div>
   )
 }
