d4b7e050b0fcdf236328e01e7ab1f41c5a2144b8
hongsujin2eeZyo <hong266770@gmail.com>
2025-07-07 20:29:00 +0900

    addRIP 제작함

--- LikeLion-Hachathon-Front/src/Visitor/AddRIP/AddRIP.jsx
+++ LikeLion-Hachathon-Front/src/Visitor/AddRIP/AddRIP.jsx
@@ -1,20 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './AddRIP.css';
 
 
 function AddRIP() {
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
 
-    // const handleLogin = () => {
-    //     navigate("/"); // 로그인 페이지로 이동
-    // }
+    const [title, setTitle] = useState(""); // 제목 상태 관리
+    const [author, setAuthor] = useState(""); // 작성자 이름 상태 관리
+    const [content, setContent] = useState(""); // 내용 상태 관리
+
+    const handleSubmit = () => {
+        // 작성 완료 버튼 클릭 시 처리 로직
+        console.log("제목:", title);
+        console.log("작성자 이름:", author);
+        console.log("내용:", content);
+        alert("글이 작성되었습니다!");
+    };
+   
 
     return(
         <div>
                  <div className="AddRIP-Top-bar">
-                 <div className="AddRIP-Logo"  >lastLink</div>
+                    <div className='backButton' onClick={() => navigate(-1)}>←</div>
+
+                    <div className="AddRIP-Logo"  >lastLink</div>
                  </div>
+                 <div className="AddRIP-Body">
+                    <div className="RIP-Title">
+                        <p>제목</p>
+                        <input 
+                        type="text" 
+                        className="Input" 
+                        value={title} 
+                        onChange={(e) => setTitle(e.target.value)} 
+                        placeholder="제목을 입력하세요" 
+                    />
+
+                    </div>
+                    <div className="RIP-Name">
+                        <p>작성자 이름</p>
+                        <input 
+                        type="text" 
+                        className="Input" 
+                        value={author} 
+                        onChange={(e) => setAuthor(e.target.value)} 
+                        placeholder="이름을 입력하세요" 
+                    />
+                    </div>
+                    <div className="RIP-content">
+                        <p>내용</p>
+                        <textarea
+                        className="TextInput" 
+                        value={content} 
+                        onChange={(e) => setContent(e.target.value)} 
+                        placeholder="내용을 입력하세요" 
+                    />
+                    </div>
+
+                    <div className="RIP-Submit">
+                    <button className="Submit-Button" onClick={handleSubmit}>
+                        작성 완료
+                    </button>
+                </div>
+                </div>
         </div>
     )
 }
