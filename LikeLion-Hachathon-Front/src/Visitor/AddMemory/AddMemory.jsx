ed73e9b0ca9f0d0ba3d2ab9f611acba124de4874
hongsujin2eeZyo <hong266770@gmail.com>
2025-07-08 16:44:11 +0900

    memoryroom,addmemory api연결중

--- LikeLion-Hachathon-Front/src/Visitor/AddMemory/AddMemory.jsx
+++ LikeLion-Hachathon-Front/src/Visitor/AddMemory/AddMemory.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import './AddMemory.css'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react';
+import axios from "axios";
 
 const AddMemory = () => {
     const navigate = useNavigate();
      const [image, setImage] = useState(null);
+     const [description, setDescription] = useState(''); 
+     const [author, setAuthor] = useState(''); 
+
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -18,7 +22,36 @@ const AddMemory = () => {
     }
   };
 
+  const handleAddMemory = async () => {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+        alert('로그인이 필요합니다.');
+        return;
+    }
 
+    try {
+        const response = await axios.post(
+            'https://lastlink.p-e.kr/room/', 
+            {
+                imageUrl: image, 
+                description: description, 
+                name: author, 
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`, // 인증 토큰
+                },
+            }
+        );
+        if (response.status === 201) {
+            alert('기억이 성공적으로 추가되었습니다.');
+            navigate(-1); // 이전 페이지로 이동
+        }
+    } catch (error) {
+        console.error('기억 추가 실패:', error);
+        alert('기억 추가에 실패했습니다. 다시 시도해주세요.');
+    }
+};
 
   return (
     <div>
@@ -46,15 +79,23 @@ const AddMemory = () => {
        <div className='AddMemoryContent'>
         <div className='AddMemoryTitle'>
             <p>사진설명</p>
-            <input type='text'className='Memorytext'/>
+            <input 
+                type='text'
+                className='Memorytext'
+                value={description} 
+                onChange={(e) => setDescription(e.target.value)}/>
         </div>
         <div className='AddMemoryAuthor'>
             <p>작성자 이름</p>
-            <input type='text'className='Memorytext'/>
+            <input 
+                type='text'
+                className='Memorytext'
+                value={author} 
+                onChange={(e) => setAuthor(e.target.value)}/>
         </div>
        </div>
       </div>
-      <button className='AddMemoryButton'>
+      <button className='AddMemoryButton' onClick={handleAddMemory}>
         기억 추가하기
       </button>
       
